7db99e09fc034e9567125fe30ab9c16e74bcfccf
John <johnartuz@hotmail.com>
2021-11-23 16:50:38 -0500

    Function that takes 2 arguments and checks if they're both equal to each other

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  //check if actual is === to expected
+  if (actual === expected) {
+    return `🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    // if actual does not === to expected return a message
+    return `✔✔✔ Assertion Failed: ${actual} !=== ${expected}`;
+  }
+};
+
+
+// console.log(assertEqual(1, 1));
+
+
+
+// loop through the arguments
+// check if each element is === to one another
+
+const eqArrays = (test1, test2) => {
+  for(let i = 0; i < test1.length; i++) {  
+    //if test[i] does not equal return false
+      if((test1[i] !== test2[i])) {   
+      return false;
+      }
+    }
+    return true;
+  }
+
+  console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
+  // console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+// const eqArrays = (test1, test2) => {
+//   for(let i = 0; i < test1.length; i++) {  
+//       if((test1[i] !== test2[i])) {   
+//       return `${false} The following element ${test1[i]} does not === to ${test2[i]}`;
+//       }
+//     console.log((`Looping---> The first array === ${test1[i]} and The second array === ${test2[i]}.`));
+//     }
+//     return `${true} Both arguments equal to each other`;
+//   }
+
