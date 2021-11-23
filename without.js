614946f0c450e7123083073f04a26292531eae2a
John <johnartuz@hotmail.com>
2021-11-23 18:05:15 -0500

    returns a new array without the element that equals to args1 && args2

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = (test1, test2) => {
+  for(let i = 0; i < test1.length; i++) {  
+    //if test[i] does not equal return false
+      if((test1[i] !== test2[i])) {   
+      return false;
+      }
+    }
+    return true;
+  }
+
+
+// Takes in two arrays and checks if they're both equal
+const assertArraysEqual = (arg1, arg2) => {
+  return `${eqArrays(arg1, arg2)}`
+};
+
+
+// Returns a new array without the element that equals to args1 && args2
+const without = (args1, args2) => {
+  let withoutItems = [];
+  for(let i = 0; i < args1.length; i++) {
+    if(args2[i] !== args1[i]) {
+      withoutItems.push(args1[i]);
+    }
+  }
+  return withoutItems;
+}
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+//test case
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
