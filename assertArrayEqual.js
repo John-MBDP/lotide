a9c2948f315d2794729f8dc3d7caa75c115c14e0
John <johnartuz@hotmail.com>
2021-11-24 10:33:13 -0500

    Change the logic for eqArrays Function

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,12 +1,17 @@
 const eqArrays = (test1, test2) => {
-  for(let i = 0; i < test1.length; i++) {  
-    //if test[i] does not equal return false
-      if((test1[i] !== test2[i])) {   
-      return false;
-      }
-    }
-    return true;
+
+  // check if the array lengths are different
+  if (test1.length !== test2.length) return false;
+
+for (let i = 0; i < test1.length; i++) {
+  //if test[i] does not equal return false
+  if ((test1[i] !== test2[i])) {
+    return false;
   }
+}
+return true;
+};
+
 
 
 // Takes in two arrays and checks if they're both equal
