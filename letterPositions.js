6323119d707aa5c8d285c89425959e79b2addd97
John <johnartuz@hotmail.com>
2021-11-24 19:12:38 -0500

    Function for returning the index of the elements within a string.  Puts them as an array in an object

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = (test1, test2) => {
+
+  // check if the array lengths are different
+  if (test1.length !== test2.length) return false;
+
+for (let i = 0; i < test1.length; i++) {
+  //if test[i] does not equal return false
+  if ((test1[i] !== test2[i])) {
+    return false;
+  }
+}
+return true;
+};
+
+
+
+// Takes in two arrays and checks if they're both equal
+const assertArraysEqual = (arg1, arg2) => {
+  return `${eqArrays(arg1, arg2)}`
+};
+
+
+
+const letterPositions = function(sentence) {
+  const noSpaces = sentence.split(" ").join('').toLowerCase();
+  const results = {};
+
+
+for(let i = 0; i < noSpaces.length; i++) {
+  if(results[noSpaces[i]]) {
+    results[noSpaces[i]].push(i);
+  } else {
+    results[noSpaces[i]] = [i];
+  }
+}
+return results;
+};
+
+const result1 = letterPositions("hello");
+
+//Test Case
+console.log(assertArraysEqual(result1.h, [1]));
