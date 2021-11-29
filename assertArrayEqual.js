b72cc3bd7582d445aadc41ebc055f228206dadf0
John <johnartuz@hotmail.com>
2021-11-29 14:11:09 -0500

    Refactored to have seperate test file

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = (test1, test2) => {
-  // check if the array lengths are different
-  if (test1.length !== test2.length) return false;
-
-  for (let i = 0; i < test1.length; i++) {
-    //if test[i] does not equal return false
-    if (test1[i] !== test2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 // Takes in two arrays and checks if they're both equal
 const assertArraysEqual = function (arg1, arg2) {
@@ -20,4 +9,4 @@ const assertArraysEqual = function (arg1, arg2) {
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual;
