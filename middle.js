6edee45c50dfa822d3973b539fc468397140642f
John <johnartuz@hotmail.com>
2021-11-29 14:27:44 -0500

    Refactored to have a seperate test file

--- middle.js
+++ middle.js
@@ -1,28 +1,6 @@
 "use strict";
-
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = (test1, test2) => {
-  // check if the array lengths are different
-  if (test1.length !== test2.length) return false;
-
-  for (let i = 0; i < test1.length; i++) {
-    //if test[i] does not equal return false
-    if (test1[i] !== test2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arg1, arg2) {
-  if (eqArrays(arg1, arg2)) {
-    return `✅✅✅ Assertation Passed: ${arg1} === ${arg2}`;
-  } else {
-    return `🔴🔴🔴 Assertation Failed: ${arg1} !== ${arg2}`;
-  }
-};
-
-// ACTUAL FUNCTION
+const assertArrayEqual = require("./assertArrayEqual");
+const eqArrays = require("./eqArrays");
 
 const middle = (arr) => {
   let newArr = [];
@@ -44,26 +22,4 @@ const middle = (arr) => {
   return newArr;
 };
 
-// Take the middle number of an array if(even)
-// console.log(assertArraysEqual(middle([3, 3, 4, 5]), ([3, 4]))); // --> True
-
-// // Take the middle number of an array if(odd)
-// console.log(assertArraysEqual(middle([3, 3, 5]), ([3]))); // --> True
-
-// Take the middle number of an array if(even)
-console.log(assertArraysEqual(middle([3, 3, 4, 5]), [3, 4, 5])); // --> False
-
-// Take the middle number of an array if(odd)
-console.log(assertArraysEqual(middle([3, 3]), [3])); // --> False
-
-//Return if the length of arr is less than 2
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-
-//Return the middle number of an odd array
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-//Return the middle number of an even array
-// console.log(middle([1, 2, 3, 4])); // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
