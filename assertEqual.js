ed21a119e71df0c11b54d35d2781adc7452ee1d9
John <johnartuz@hotmail.com>
2021-11-29 13:31:56 -0500

    Refactored to seperated testing

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,13 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   //check if actual is === to expected
-  if (actual === expected) {
-    return `🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    // if actual does not === to expected return a message
-    return `✔✔✔ Assertion Failed: ${actual} !=== ${expected}`;
+  if (actual !== expected) {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`;
   }
+  // if actual does not === to expected return a message
+  return `✔✔✔Assertion Passed: ${actual} === ${expected}`;
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+// console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+// console.log(assertEqual(1, 1));
+
+module.exports = assertEqual;
