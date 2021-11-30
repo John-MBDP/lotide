9dfd61ca98890743886d78d2f05bad17db48144d
John <johnartuz@hotmail.com>
2021-11-29 21:45:56 -0500

    Refactored codes to integrate strict mode and using import and exports to link files

--- findkey.js
+++ findkey.js
@@ -1,11 +1,6 @@
-const assertEqual = function (actual, expected) {
-  //check if actual is === to expected
-  if (actual !== expected) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`;
-  }
-  // if actual does not === to expected return a message
-  return `✔✔✔Assertion Passed: ${actual} === ${expected}`;
-};
+"use strict";
+
+const assertEqual = require("./assertEqual");
 
 const findKey = function (obj, callback) {
   for (const key in obj) {
@@ -15,16 +10,4 @@ const findKey = function (obj, callback) {
   }
 };
 
-const results1 = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-); // => "noma"
-
-console.log(assertEqual(results1, "noma"));
+module.exports = findKey;
